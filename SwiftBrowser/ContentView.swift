d6a4ab7b40ded402726a30a14101d165c08be33c
Ahmad Alhashemi <ahmad@ahmadh.com>
2024-03-17 03:19:06 +0300

    Initial Commit

---
+++ SwiftBrowser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftBrowser
+//
+//  Created by Ahmad Alhashemi on 17/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftBrowser/SwiftBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBrowserApp.swift
+//  SwiftBrowser
+//
+//  Created by Ahmad Alhashemi on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
